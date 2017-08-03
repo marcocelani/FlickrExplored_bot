19c70b0f0dd163714c27ef544cd847316fb17c9b
marcocelani <ettore.galluccio@enel.com>
2017-08-03 14:37:53 +0200

    New scraping algorithm.

--- config.example.js
+++ config.example.js
@@ -2,7 +2,10 @@ var config = {
     BOT_TOKEN           : 'YOUR_TOKEN',
     FLICKR_KEY          : 'YOUR_FLICKR_KEY',
     TELEGRAM_USERNAME   : 'YOUR_USERNAME',
-    APP_NAME            : 'APP_NAME'
+    APP_NAME            : 'APP_NAME',
+    IMGS_REFRESH_TIME   : (1000 * 60 * 60 * 24), /* 24h */
+    IMGS_ARR_REFRESH    : (1000 * 60 * 60 * 23), /* 23h */
+    DAY_BEFORE          : 28, /* FULL MOON =) */
 }
 
 exports.config = config;
---
+++ flickrexplored.js
@@ -0,0 +1,261 @@
+const TeleBot = require('telebot');
+const config = require('./config.js').config;
+const rp = require('request-promise');
+const async = require('async');
+const htmlparser = require('htmlparser2');
+const moment = require('moment');
+
+const bot = new TeleBot({
+    token: config.BOT_TOKEN
+});
+
+const flickrObj = {
+    ENDPOINT    : 'https://api.flickr.com/services/rest/',
+    METHODS     : ['flickr.interestingness.getList', 'flickr.people.getInfo', 'flickr.photos.getInfo'],
+    API_KEY     : config.FLICKR_KEY,
+    EXTRAS      : ['path_alias'],
+    PER_PAGE    : 1, /* Flickr max default: 500 */
+    FORMAT      : 'json',
+    NOJSONCB    : 1,
+    FLICKR_EXPLORE_URL: 'https://www.flickr.com/explore/'
+};
+
+/** Core Data Structure **/
+var imgsObj = {
+    lastUpdate : null,
+    imgs: [],
+    scrapeInProgress: false
+};
+/*************************/
+
+var about = function(){
+    return `${config.APP_NAME} made by @${config.TELEGRAM_USERNAME}.`;
+};
+
+var usage = function() {
+    return `Type /photo for pick a photo.
+Type /help for show help.
+Type /about for show info.`; 
+};
+
+var getWelcome = function(firstName) {
+   return `Welcome ${(firstName) ? firstName : 'user'}!
+My mission is to show you the most Flickr's interesting photos in a randomic way.
+${usage()}`;
+};
+
+/* get randomic number between 0 and max. */
+var getRandomic = function(upperBound){
+    if(upperBound && typeof(upperBound) !== 'number')
+        return 0;
+    let min = 0;
+    let max = Math.floor(upperBound);
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getMsgError = function(response){
+    if(response){
+        return `Something goes wrong. Flickr response:${response.message}, code:${response.code}`;
+    }
+    return '';
+};
+
+var log = function(type, msg){
+    if(!type)
+        type = 'INFO';
+    console.log(`[${type}][${moment().format('DD/MM/YYYY hh:mm:ss')}] ${msg}`);
+};
+
+var logInfo = function(msg){
+    log('INFO', msg);
+};
+
+var logErr = function(msg){
+    log('ERR', msg);
+};
+
+var scrapeEngine = function(name, attribs, imgsArr){
+    if(name === 'div' 
+        && attribs.style)
+    {
+        let tokens = attribs.style.split(':');
+        for(let i=0; i<tokens.length; ++i){
+            let token = tokens[i].trim();
+            if(token.includes('url')
+                && token.length > 4)
+            {
+                let img_url = token.substring(4, token.length - 1);
+                if(img_url.endsWith('.jpg')
+                    || img_url.endsWith('.JPG'))
+                {
+                    let img = img_url.split('/');
+                    if(img.length == 0) {
+                        logErrS('img has no length.');
+                        return;
+                    }
+                    img = img[img.length-1];
+                    let img_id = img.split('_');
+                    if(img_id.length == 0){
+                        logErr('img_id has no length.');
+                        return;
+                    }
+                    imgsArr.push(img_id[0]);
+                }
+            }
+        }
+    }
+}
+
+var getPhotoIdsEngine = function(rpObj, dateStr, task_id){
+    if(!rpObj){
+        return function(cb){
+            cb(new Error(`Wrong rpObj.`));
+        };
+    }
+    return function(cb) {
+        logInfo(`task ${task_id} started.`)
+        let imgsArr = [];
+        let parser = new htmlparser.Parser(
+        {
+           onopentag: function(name, attribs){
+               scrapeEngine(name, attribs, imgsArr);
+           }
+        });
+        rp(rpObj)
+        .then( response => {
+            parser.write(response);
+            parser.end();
+            logInfo(`task ${task_id} ended.`);
+            cb(null, { date: dateStr, imgsArr: imgsArr });
+        })
+        .catch( err => {
+            console.log(err);
+            cb(err);
+        });
+    };
+};
+
+var scrapeImg = function() {
+    if(imgsObj.scrapeInProgress){
+        logInfo(`Another scrape is in progress.`);
+        return;
+    }
+    let mDate = moment(new Date());
+
+    if(imgsObj.lastUpdate){
+        if(mDate.format('YYYY/DD/MM') === imgsObj.lastUpdate.format('YYYY/DD/MM')) {
+            logInfo(`Skipping same day update.`);
+            return;
+        }
+    }
+
+    imgsObj.scrapeInProgress = true;
+
+    let flickrUrlsArr = [];
+    let dayBefore = config.DAY_BEFORE - imgsObj.imgs.length;
+    if(dayBefore < 0){
+        logErr(`dayBefore:${dayBefore}. Negative values found. I'm restoring imgs array.`);
+        imgsObj.imgs = [];
+        dayBefore = config.DAY_BEFORE;
+    }
+    if(dayBefore != 0){
+        logInfo(`imgs needs update. dayBefore:${dayBefore}`)
+        let tasksArr = [];
+        for(let i=0; i<dayBefore; ++i) {
+            mDateStr = mDate.subtract(1, 'days').format('YYYY/MM/DD');
+            tasksArr.push(getPhotoIdsEngine({ uri : flickrObj.FLICKR_EXPLORE_URL + 
+                                                    mDateStr }, mDateStr, i));
+        }
+        logInfo('starting parallel tasks...');
+        async.parallelLimit(tasksArr, 2,
+            function(err, result){  
+                if(err){
+                    logErr(`Something goes wrong:${err.message}.`);
+                    return;
+                }
+                logInfo(`tasksArr ended.`);
+                for(let i=result.length-1; i>=0; --i){
+                    imgsObj.imgs.push(result[i]);
+                }
+                imgsObj.lastUpdate = moment();
+                imgsObj.scrapeInProgress = false;
+            }
+        );
+    } 
+};
+
+var getPhotoV2 = function(msg){
+    if(imgsObj.imgs.length == 0){
+        logErr(`imgs is empty`);
+        return;
+    }
+    var imgsIds = imgsObj.imgs[getRandomic(imgsObj.imgs.length - 1)];
+    var img_id = imgsIds.imgsArr[getRandomic(imgsIds.imgsArr.length - 1)]
+
+    rpOpt = {
+        uri: flickrObj.ENDPOINT,
+        qs: {
+                method: flickrObj.METHODS[2],
+                api_key: flickrObj.API_KEY,
+                photo_id: img_id,
+                format: flickrObj.FORMAT,
+                nojsoncallback: flickrObj.NOJSONCB,
+        },
+        json: true
+    };
+    
+    rp(rpOpt)
+    .then( response => {
+        if(response.stat === 'fail'){
+            msg.reply.text(getMsgError(response));
+            return;
+        }
+        if(response.photo
+            && response.photo.urls
+            && response.photo.urls.url 
+            && response.photo.urls.url[0]
+            && response.photo.urls.url[0]._content)
+            msg.reply.text(response.photo.urls.url[0]._content);
+    }).catch(err => {
+        console.log(err);
+        logErr(`Something goes wrong.`);
+    });
+};
+
+var removeFirstItem = function() {
+    if(imgsObj.lastUpdate){
+        if(imgsObj.lastUpdate.format('YYYY/DD/MM') !== moment().format('YYYY/DD/MM')) {
+            if(imgsObj.scrapeInProgress){
+                logInfo(`Scrape in progress.`);
+                return;
+            }
+            logInfo('Removing first element.');
+            if(imgsObj.imgs.length > 0){
+                imgsObj.imgs.pop();
+            } else {
+                logErr('imgs is empty.');
+            }       
+        }
+    }
+};
+
+var init = function(){
+    scrapeImg();
+
+    setInterval(() => {
+        removeFirstItem()
+    },
+        config.IMGS_ARR_REFRESH
+    );
+    setInterval(() => {
+        scrapeImg()
+    },config.IMGS_REFRESH_TIME)
+
+    bot.on(['/start'], (msg) =>  msg.reply.text(getWelcome(msg.from.first_name)));
+    bot.on(['/photo'], (msg) => getPhotoV2(msg) );
+    bot.on('/help', (msg) => msg.reply.text(usage()));
+    bot.on('/about', (msg) => msg.reply.text(about()));
+    bot.start();
+};
+
+init();
